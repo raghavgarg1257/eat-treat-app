ada895cb0c83d3a964c6ac7a8e7f2c389bef3147
raghav garg <ansraghav100@gmail.com>
2017-04-13 03:22:02 +0530

    added filed

---
+++ README.md
@@ -0,0 +1,6 @@
+# Eat Treat - Sample App
+A very simple mockup of 9Gag TV, made in ReactJS.
+
+## Steps to Run
+- npm i
+- npm start
---
+++ config.js
@@ -0,0 +1,2 @@
+process.env['API_URL'] = "https://www.googleapis.com/youtube/v3";
+process.env['API_KEY'] = "AIzaSyCtmOdiuijgBaaYw-f20ecuzeoLjjxPXw0"; // you should enter your own google app key
