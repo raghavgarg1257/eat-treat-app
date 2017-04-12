f8da3300aec7cadbbd49ade05ee0fb10e66f68a3
raghav garg <ansraghav100@gmail.com>
2017-04-13 03:55:56 +0530

    try again heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const express = require('express')
+
+    const app = express()
+    const indexPath = path.join(__dirname, '/../index.html')
+    const publicPath = express.static(path.join(__dirname, '../public'))
+
+    app.use('/public', publicPath)
+    app.get('/', function (_, res) { res.sendFile(indexPath) })
+
+
+app.listen(80)
